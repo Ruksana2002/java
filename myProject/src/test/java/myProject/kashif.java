59d46eb55ac8782639b2f8f22565e650c3e02965
user <user@192.168.1.18>
2023-08-27 16:14:53 +0530

    1st commit

---
+++ myProject/src/test/java/myProject/kashif.java
@@ -0,0 +1,20 @@
+package myProject;
+
+public class kashif {
+
+	public static void main(String[] args) {
+		int n=9;
+		int count=0;
+		for (int i = 1; i <=n/2; i++) {
+			if (n%i==0) {
+				count++;				
+			}
+		}
+		if (count==1) 
+			System.out.println("is prime");
+		
+		else 
+			System.out.println("not prime");
+		
+	}
+}
